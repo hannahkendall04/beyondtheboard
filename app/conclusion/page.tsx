4ccd3acdd25eee816b85efae0dd0e4551a352a86
Hannah Kendall <hbkendall@outlook.com>
2026-03-01 14:13:08 -0800

    add conclusion

--- app/conclusion/page.tsx
+++ app/conclusion/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 import GenericPage from "@/components/GenericPage";
 import TextSection from "@/components/TextSection";
-import { Box, Link } from "@mui/material";
-import TableauComponent from "@/components/TableauComponent";
+import PhotoSection from "@/components/PhotoSection";
 import NarrativeNav from "@/components/NarrativeNav";
+import { narrativeContent } from "@/public/data/narrative-content";
 
 
 export default function ConclusionPage() {
@@ -13,6 +13,19 @@ export default function ConclusionPage() {
         route="conclusion" 
         pageTitle="Conclusion" 
       >
+        <TextSection 
+          sectionHeader="Conclusion"
+          sectionSubheader={narrativeContent.conclusion.rq}
+          sectionContent={[narrativeContent.conclusion.analysis_1]}
+        />
+        <PhotoSection 
+          imageUrl="/images/graduation.jpg"
+          sectionText={[narrativeContent.conclusion.analysis_2]}
+          direction="right"
+        />
+        <TextSection 
+          sectionContent={[narrativeContent.conclusion.analysis_3]}
+        />
         <NarrativeNav
           prevHref="/narrative_3"
           prevLabel="Research Question 3"
